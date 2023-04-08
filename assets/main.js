02fd7d377ddeb65d840b18156316f4aa15a63036
Martijn van Turnhout <martijn.van.turnhout@pharmapartners.nl>
2023-04-08 19:14:50 +0200

    feat: added hover effects

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,17 @@
-const menuItems = document.querySelectorAll(".menu > li")
-const submenuItems = document.querySelectorAll(".menu > li > ul > li");
+const menuItems = document.querySelectorAll(".menu > li");
+const submenuItems = document.querySelectorAll(".submenu > li");
 
 menuItems.forEach(menuItem => {
     const submenu = menuItem.querySelector("ul");
-    menuItem.addEventListener('click', () => {
-        submenu.classList.toggle('open')
+    menuItem.addEventListener("click", () => {
+        submenu.classList.toggle("open")
     })
-})
+});
+
 submenuItems.forEach(subMenuItem => {
-    subMenuItem.addEventListener('click', (event) => {
+    subMenuItem.addEventListener('click', event => {
         event.stopPropagation();
         const trimmedText = event.target.textContent.trim();
         alert(trimmedText)
     })
-})
+});
