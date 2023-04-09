292387791c76b18a14bfe9e12afd3ac04bacd205
Martijn van Turnhout <martijn.van.turnhout@pharmapartners.nl>
2023-04-09 20:27:51 +0200

    feat: added automatic closing of other submenus

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,27 @@
 const menuItems = document.querySelectorAll(".menu > li");
 const submenuItems = document.querySelectorAll(".submenu > li");
+const closeOtherSubmenus = (index) => {
+  const currentMenuItemIndex = index + 1;
+  const otherMenuItems = document.querySelectorAll(
+    `.menu > li:not(:nth-child(${currentMenuItemIndex}))`
+  );
+  otherMenuItems.forEach((otherMenuItem) => {
+    otherMenuItem.querySelector(".submenu").classList.remove("open");
+  });
+};
 
-menuItems.forEach(menuItem => {
-    const submenu = menuItem.querySelector("ul");
-    menuItem.addEventListener("click", () => {
-        submenu.classList.toggle("open")
-    })
+menuItems.forEach((menuItem, index) => {
+  const submenu = menuItem.querySelector("ul");
+  menuItem.addEventListener("click", () => {
+    closeOtherSubmenus(index);
+    submenu.classList.toggle("open");
+  });
 });
 
-submenuItems.forEach(subMenuItem => {
-    subMenuItem.addEventListener('click', event => {
-        event.stopPropagation();
-        const trimmedText = event.target.textContent.trim();
-        alert(trimmedText)
-    })
+submenuItems.forEach((subMenuItem) => {
+  subMenuItem.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const trimmedText = event.target.textContent.trim();
+    alert(trimmedText);
+  });
 });
